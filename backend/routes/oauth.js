36efecccabbc43ba3ef6449306f894ec615ec29d
michaeldsdomingo <michael.ds.domingo@gmail.com>
2019-09-08 00:19:04 -0700

    axios post

--- backend/routes/oauth.js
+++ backend/routes/oauth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 const router = express.Router();
 const qs = require('qs');
+const axios = require('axios');
 
 const clientID = 'dj0yJmk9NmMwb3doYmFxMGF5JmQ9WVdrOVdHTlFjR2xyTm1jbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWIx';
 const clientSecret = '0e6790c003224688cc9ca98002307af88a9be5b4';
@@ -26,31 +27,48 @@ router.get('/login', (req,res) => {
 // After the user authenticates, he will be redirected to this route
 // which will send a post request to yahoo and retrieve the access and refresh token
 router.get('/redirect', (req,res) => {
+    let whatever;
     let accessCode = req.query.code;
     let bodyParams = {'grant_type': 'authorization_code', 'redirect_uri': redirectUri, 'code': accessCode};
+    axios({
+        url: accessTokenURL,
+        method: 'post',
+        headers: {
+            'Authorization': `Basic ${clientHash}`,
+            'Content-Type': `application/x-www-form-urlencoded`,
+            
+        },
+        response_type: 'json'
+    })
+    .then( (response) => {
+        whatever = response.data;
+    })
+    .catch( (error) => {
+        whatever = error;
+    })
+    // let xml = new XMLHttpRequest();
+    // xml.open("POST", accessTokenURL,true);
+    // xml.setRequestHeader('Authorization','Basic ' + clientHash);
+    // xml.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
     
-    let xml = new XMLHttpRequest();
-    xml.open("POST", accessTokenURL,true);
-    xml.setRequestHeader('Authorization','Basic ' + clientHash);
-    xml.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-    
-    xml.onreadystatechange=function(){
-        if( xml.readyState == 4 && xml.status == 200) {
-            let json = JSON.parse(xml.responseText);
-            accessToken = json['access_token'];
-            let refreshToken = json['refresh_token'];
-            console.log(json);
-            var string = encodeURIComponent(accessToken);
-        }
-        else {
-            console.log(xml.status);
-            console.log(xml.responseText);
-            console.log("Getting access token Unsucessful")
-        }
-    };
-    xml.send(qs.stringify(bodyParams)); 
+    // xml.onreadystatechange=function(){
+    //     if( xml.readyState == 4 && xml.status == 200) {
+    //         let json = JSON.parse(xml.responseText);
+    //         accessToken = json['access_token'];
+    //         let refreshToken = json['refresh_token'];
+    //         console.log(json);
+    //         var string = encodeURIComponent(accessToken);
+    //     }
+    //     else {
+    //         console.log(xml.status);
+    //         console.log(xml.responseText);
+    //         console.log("Getting access token Unsucessful")
+    //     }
+    // };
+    // xml.send(qs.stringify(bodyParams)); 
     // res.redirect('https://nbatrader.michaeldomingo.dev');
-    res.send(accessToken);
+    // res.send(accessToken);
+    res.send(whatever);
 })
 
 
