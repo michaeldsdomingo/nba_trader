44b5b01a77dc9e857ba771bd4f8f7322d40f9811
michaeldsdomingo <michael.ds.domingo@gmail.com>
2019-09-08 11:18:37 -0700

    fixed client hash

--- backend/routes/oauth.js
+++ backend/routes/oauth.js
@@ -9,7 +9,7 @@ const clientSecret = '0e6790c003224688cc9ca98002307af88a9be5b4';
 const redirectUri = 'https://nbatrader.michaeldomingo.dev/oauth/redirect';
 const authURL = 'https://api.login.yahoo.com/oauth2/request_auth';
 const accessTokenURL = 'https://api.login.yahoo.com/oauth2/get_token';
-const clientHash = 'MGU2NzkwYzAwMzIyNDY4OGNjOWNhOTgwMDIzMDdhZjg4YTliZTViNA==';
+const clientHash = 'ZGoweUptazlObU13YjNkb1ltRnhNR0Y1Sm1ROVdWZHJPVmRIVGxGalIyeHlUbTFqYldOSGJ6bE5RUzB0Sm5NOVkyOXVjM1Z0WlhKelpXTnlaWFFtYzNZOU1DWjRQV0l4OjBlNjc5MGMwMDMyMjQ2ODhjYzljYTk4MDAyMzA3YWY4OGE5YmU1YjQ=';
 
 var accessToken = '';
 
@@ -51,29 +51,29 @@ router.all('/redirect', (req,res) => {
     // .catch( (error) => {
     //     whatever = error;
     // })
-    // let xml = new XMLHttpRequest();
-    // xml.open("POST", accessTokenURL,true);
-    // xml.setRequestHeader('Authorization','Basic ' + clientHash);
-    // xml.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+    let xml = new XMLHttpRequest();
+    xml.open("POST", accessTokenURL,true);
+    xml.setRequestHeader('Authorization','Basic ' + clientHash);
+    xml.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
     
-    // xml.onreadystatechange=function(){
-    //     if( xml.readyState == 4 && xml.status == 200) {
-    //         let json = JSON.parse(xml.responseText);
-    //         accessToken = json['access_token'];
-    //         let refreshToken = json['refresh_token'];
-    //         console.log(json);
-    //         var string = encodeURIComponent(accessToken);
-    //     }
-    //     else {
-    //         console.log(xml.status);
-    //         console.log(xml.responseText);
-    //         console.log("Getting access token Unsucessful")
-    //     }
-    // };
-    // xml.send(qs.stringify(bodyParams)); 
+    xml.onreadystatechange=function(){
+        if( xml.readyState == 4 && xml.status == 200) {
+            let json = JSON.parse(xml.responseText);
+            accessToken = json['access_token'];
+            let refreshToken = json['refresh_token'];
+            console.log(json);
+            var string = encodeURIComponent(accessToken);
+        }
+        else {
+            console.log(xml.status);
+            console.log(xml.responseText);
+            console.log("Getting access token Unsucessful")
+        }
+    };
+    xml.send(qs.stringify(bodyParams)); 
     // res.redirect('https://nbatrader.michaeldomingo.dev');
-    // res.send(accessToken);
-    res.send("something");
+    res.send(accessToken);
+    // res.send("something");
 })
 
 
