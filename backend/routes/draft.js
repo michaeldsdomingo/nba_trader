1036f9689bf8f7ac39d067fb53239693b8b7fdcb
michaeldsdomingo <michael.ds.domingo@gmail.com>
2019-09-15 01:26:35 -0700

    added json converter

--- backend/routes/draft.js
+++ backend/routes/draft.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const convert = require('xml-js');
 
 router.get('/projections', (req,res) => {
     axios.get('https://www.fantasybasketballnerd.com/service/draft-projections')
     .then( (response) => {
-        res.send(response.data);
+        var json = convert.xml2json(response.data, {compact: true, spaces: 4})
+        res.send(json);
     })
     .catch( (error) => {
         res.send(error);
