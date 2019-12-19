87637678e1ae8f6ee61852d542fe481c76ac3e45
michaeldsdomingo <michael.ds.domingo@gmail.com>
2019-12-19 00:51:27 -0800

    env testing

--- backend/routes/oauth.js
+++ backend/routes/oauth.js
@@ -19,6 +19,7 @@ var accessToken = '';
 
 // When the user clicks on login, it will redirect to yahoo's login plus parameters
 router.get('/login', (req,res) => {
+    console.log(new Date().toLocaleString('en-US'))
     console.log(process.env.REDIRECT_URI);
     var queryParams = qs.stringify({
         client_id: clientID,
